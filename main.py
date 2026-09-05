8333fcf16462d883c50546991477a7924dc9922c
Pratik5676 <pratikvsalunkhe924@gmail.com>
2026-09-05 21:46:46 +0530

    tested the pipeline

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from src.tools.tools import web_search, scrape_url
+# from src.tools.tools import web_search, scrape_url
+from src.pipelines.pipeline import run_research_pipeline
 
 
 
-result = web_search.invoke("Can tell the todays whether")
-print(result)
+# result = web_search.invoke("Can tell the todays whether")
+# print(result)
 
-result2 = scrape_url("https://www.anthropic.com/news")
-print(result2)
+# result2 = scrape_url("https://www.anthropic.com/news")
+# print(result2)
+
+
+topic = "The impact of AI on job market 2024"
+run_research_pipeline(topic)
